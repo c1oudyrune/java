79b3c9c2c9fe30bb518609b4d466b9bf9e5361e8
c1oudyrune <c1oudyrune@mail.ru>
2026-03-30 08:50:01 +0300

    Update send_telegram_notification.py

--- send_telegram_notification.py
+++ send_telegram_notification.py
@@ -1,1 +1,21 @@
+import os
+import requests
 
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+MESSAGE = os.getenv("MESSAGE")
+
+def send_message():
+    url = f"https://api.telegram.org/bot{8689239947:AAHbClkp-RpQUdVFXgtlFXtkZE_cv3QEAps}/sendMessage"
+    payload = {
+        'chat_id': CHAT_ID,
+        'text': MESSAGE
+    }
+    response = requests.post(url, data=payload)
+    if response.status_code != 200:
+        print("Ошибка при отправке сообщения:", response.text)
+    else:
+        print("Сообщение успешно отправлено!")
+
+if __name__ == "__main__":
+    send_message()
